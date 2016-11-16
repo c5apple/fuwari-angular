853ce6411b644a5c797dba3ad4bc972b84373401
c5apple <c5apple_party@yahoo.co.jp>
2016-11-17 00:14:21 +0900

    add command to build

---
+++ script/make_html.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASEDIR=`dirname $0`
+DISTDIR=$BASEDIR/../dist
+
+csvfile=$BASEDIR/../src/meta.csv
+for line in `cat ${csvfile} | sed -e '1d'`
+do
+  arg1=`echo ${line} | cut -d ',' -f 1`
+  arg2=`echo ${line} | cut -d ',' -f 2`
+  arg3=`echo ${line} | cut -d ',' -f 3`
+
+  name=${arg1}.html
+  title="<title>"${arg2}" - ふわりAngular<\/title>"
+  description="<meta name=\"description\" content=\""${arg3}"\" \/>"
+
+  cat $DISTDIR/index.html | sed -e "s/<title>.*$/${title}/g;s/<meta name=\"description\".*$/${description}/g" > $DISTDIR/$name
+done
