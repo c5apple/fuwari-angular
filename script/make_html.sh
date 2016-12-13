1d1201f503a13ddd5c9f7e5e9dada97929047454
c5apple <c5apple_party@yahoo.co.jp>
2016-12-13 22:49:03 +0900

    ビルドコマンド修正 CDN

--- script/make_html.sh
+++ script/make_html.sh
@@ -3,6 +3,9 @@
 BASEDIR=`dirname $0`
 DISTDIR=$BASEDIR/../dist
 
+URL="http:\/\/www.banana-juice.com\/angular\/"
+CDNURL="http:\/\/cdn.banana-juice.com\/angular\/js\/"
+
 csvfile=$BASEDIR/../src/meta.csv
 for line in `cat ${csvfile} | sed -e '1d'`
 do
@@ -13,7 +16,12 @@ do
   name=${arg1}.html
   title="<title>"${arg2}" - ふわりAngular<\/title>"
   description="<meta name=\"description\" content=\""${arg3}"\" \/>"
-  url="http:\/\/www.banana-juice.com\/angular\/"
 
-  cat $DISTDIR/index.html | sed -e "s/<title>.*$/${title}/g;s/<meta name=\"description\".*$/${description}/g;s/${url}/${url}${name}/g" > $DISTDIR/$name
+  sedstr="s/<title>.*$/${title}/g"
+  sedstr="${sedstr};s/<meta name=\"description\".*$/${description}/g"
+  sedstr="${sedstr};s/${URL}/${URL}${name}/g"
+  sedstr="${sedstr};s/src=\"inline.js/src=\"${CDNURL}inline.js/g"
+  sedstr="${sedstr};s/src=\"main./src=\"${CDNURL}main./g"
+
+  cat $DISTDIR/index.html | sed -e "${sedstr}" > $DISTDIR/$name
 done
